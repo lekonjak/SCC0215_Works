b80c44ca476dcb8ce8d23f26a5b32c9ba3369182
lekonjak <ricardodogc@gmail.com>
2020-04-29 16:03:54 -0300

    (*) Fixed: misplaced typedef, incorrect variable type inside struct reg for estadoMae/Bebe.

--- t1/crud.h
+++ t1/crud.h
@@ -1,18 +1,33 @@
-#ifndef _CRUD_H_
-#define _CRUD_H_
+#ifndef CRUD_H
+#define CRUD_H
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-typedef struct reg REG;
-typedef struct head HEAD;
-	// writting registry/header to binary
+#include<string.h>
+
+#define MAX_SIZE 97
+#define ESTADO_SIZE 2
+#define GETLINE_RECOMMENDED_SIZE 120
+#define SIZEOF_REG 128
+
+typedef struct reg{
+	int sizeCidadeMae, sizeCidadeBb, idNascimento, idadeMae, dataNasc, sexoBebe;
+	char cidadeMae[MAX_SIZE], cidadeBebe[MAX_SIZE], estadoMae[ESTADO_SIZE], estadoBebe[ESTADO_SIZE];
+}REG;
+
+typedef struct head{
+	char status;
+	int RRNproxRegistro, numeroRegistrosInseridos, numeroRegistrosRemovidos, numeroRegistrosAtualizado;
+}HEAD;
+
+    // writting registry/header to binary
 int bwrite_reg(FILE *fp, REG *reg);
 int bwrite_head(FILE *fp, HEAD *head);
-	// reading register from file
-int fread_reg(FILE *fp);
+	// reading register from FILE
+int fread_reg(FILE *fp, REG *reg);
 	// reading register from binary
-int bread_reg(FILE *fp);
+int bread_reg(FILE *fp, REG *reg);
 	// miscellanea
 int print_reg(REG *reg);
 
