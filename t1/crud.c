7fd8800ae7faa901a806eee7d84f998cd157d2c1
lekonjak <ricardodogc@gmail.com>
2020-05-02 11:04:07 -0300

    (*) Parcial implementation of bwrite_reg, bwrite_head and fread_reg. Being fread_reg incomplete and remaining functions untested.

--- t1/crud.c
+++ t1/crud.c
@@ -1,34 +1,68 @@
 #include"crud.h"
-#define sizeofreg 128
 
-struct reg{
-	int sizeCidadeMae, sizeCidadeBb, idNascimento, idadeMae, dataNasc, sexoBebe, estadoMae, estadoBebe;
-	char cidadeMae[97], cidadeBebe[97];
-};
+    // writting registry/header to binary
+int bwrite_reg(FILE *fp, REG *reg){
 
-struct head{
-	char status;
-	int RRNproxRegistro, numeroRegistrosInseridos, numeroRegistrosRemovidos, numeroRegistrosAtualizado;
-};
+    fwrite(reg->sizeCidadeMae, sizeof(int), 1 ,fp);
+    fwrite(reg->sizeCidadeBebe, sizeof(int), 1 ,fp);
 
-int fwrite_reg(FILE *fp, REG *reg){
-	return 0;
+    if(reg->sizeCidadeMae)
+        fwrite(reg->cidadeMae, sizeof(char), reg->sizeCidadeMae, fp);
+    if(reg->sizeCidadeBebe)
+        fwrite(reg->cidadeBebe, sizeof(char), reg->sizeCidadeBebe, fp);
+        // fill with $
+    fwrite("$", sizeof(char), MAX_SIZE-(reg->sizeCidadeMae + reg->sizeCidadeBebe), fp); 
+        // other 
+    fwrite(reg->idNascimento, sizeof(int), 1, fp);
+    fwrite(reg->idadeMae, sizeof(int), 1, fp);
+    fwrite(reg->dataNascimento, sizeof(char), NASC_SIZE, fp);
+    fwrite(reg->sexoBebe, sizeof(char), 1, fp);
+    fwrite(reg->estadoMae, sizeof(char), ESTADO_SIZE, fp);
+    fwrite(reg->estadoBebe, sizeof(char), ESTADO_SIZE, fp);
+        // come back later
+    return 0;
 }
-
-int fread_reg(FILE *fp){
-		return 0;
+int bwrite_head(FILE *fp, HEAD *head){
+        // saving pointer location
+    long ft = ftell(fp);
+        // going to file first position
+    fseek(fp, 0, SEEK_SET);
+        // writting status byte
+    fwrite(&head->status, sizeof(char), 1, fp);
+        // writting other variables
+    fwrite(&head->RRNproxRegistro, sizeof(int), 1, fp);
+    fwrite(&head->numeroRegistrosInseridos, sizeof(int), 1, fp);
+    fwrite(&head->numeroRegistrosRemovidos, sizeof(int), 1, fp);
+    fwrite(&head->numeroRegistrosAtualizado, sizeof(int), 1, fp);
+        // fill file
+    fwrite("$", sizeof(char), HEAD_EMPTY_BYTES, fp); 
+        // returning fp to where it was
+    fseek(fp, ft, SEEK_SET);
+    return 0;
 }
-
+	// reading register from file
+int fread_reg(FILE *fp, REG *reg ){
+        //  easiest way - getline + strsep
+    char *line = NULL, *aux = NULL;
+    size_t size = GETLINE_RECOMMENDED_SIZE;
+        //  reading entire line
+    getline(&line, &size, fp);
+        //  extracting tokens with strsep
+    
+    free(line);
+    return 0;
+}
+	// reading register from binary
+int bread_reg(FILE *fp, REG *reg ){
+    return 0;
+}
+	// miscellanea
 int print_reg(REG *reg){
-		return 0;
+    return 0;
 }
 
 int erase_reg(REG *reg){
-	/*
-	 *	Libero aqui as partes da estrutura de dados utilizada.
-	 * */
-	free(reg);
-	return 0;	
+    return 0;
 }
 
 
