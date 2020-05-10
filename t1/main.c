ef22d7603e7c30b056af5bbd5e6c1445d255cf73
lekonjak <ricardodogc@gmail.com>
2020-05-09 23:51:34 -0300

    (*) Commenting some functions for testing. binarioNaTela function call added at line 58

--- t1/main.c
+++ t1/main.c
@@ -1,20 +1,22 @@
 #include"crud.h"
+#include"binarioNaTela.h"
 
 int csv2bin(char *csv, char *bin){
         // opening files pointers
 	FILE *input = fopen(csv,"r"), *output = fopen(bin, "w+b");
+    printf("'%s' '%s'", csv, bin);
 	    // starting initial registers to zero
     HEAD head = {0};
     REG reg = {0};
         // writting header into binary file
     bwrite_head(output, &head);
         // writting new registers until EOF
-    while(fread_reg(input,&reg)){
-            // counting regs 
-        head.numeroRegistrosInseridos++;
-            // writting to binary file
-        bwrite_reg(output, &reg);
-    }
+   // while(fread_reg(input,&reg)){
+   //         // counting regs 
+   //     head.numeroRegistrosInseridos++;
+   //         // writting to binary file
+   //     bwrite_reg(output, &reg);
+   // }
         // updating status byte
     head.status = 1; 
         // writting header again, with recent values and 
@@ -28,6 +30,9 @@ int csv2bin(char *csv, char *bin){
 int bin2screen(char *bin){
 	//to do
 	FILE *fp = fopen(bin, "rb");
+    
+    HEAD head = {0};
+    bread_head(fp, &head);
 
 	fclose(fp);
 	return 0;
@@ -36,18 +41,21 @@ int bin2screen(char *bin){
 int main(void){
         // setting getline variables to read input
 	size_t size = GETLINE_RECOMMENDED_SIZE;
-	char *args = NULL;
+	char *args = NULL, *in = NULL, *out = NULL;
 	int op = 0;
         // reading input
 	getline(&args, &size, stdin);
+    printf("%s", args);
         // casting operation to integer
         //      to-do: using sscanf
 	    op = atoi(strtok(args," \n"));
-
         // Operation selection
 	if( op == 1 ){
             // reads csv file and output to binary file
-		csv2bin(strtok(NULL," \n"),strtok(NULL," \n"));
+        in = strtok(NULL, " \n");
+        out = strtok(NULL, " \n");
+		csv2bin(in, out);
+        binarioNaTela(out);
 	}else if( op == 2 ){
             // reads binary file and outputs to stdout
 		bin2screen(strtok(NULL, " \n"));	
